fc799b797ffff0bb3a90988deee44497a94b5f9b
Omar Silva <Omar@wolverine.gateway.2wire.net>
2016-08-21 04:09:33 -0500

    Integrating angular 2 material design

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdToolbarModule } from '@angular2-material/toolbar'
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,
+    MdCardModule,
+    MdButtonModule,
+    MdToolbarModule,
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
